56a0e4a76d7a42da06724321871adb1d5e3515d9
Mario Young <maye.co@gmail.com>
2014-11-27 02:15:07 -0500

    remove default comment

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -24,11 +24,6 @@ class Configuration implements ConfigurationInterface
                 ->scalarNode('generator_class')->defaultValue('Vivait\StringGeneratorBundle\Generator\StringGenerator')->end()
             ;
 
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
         return $treeBuilder;
     }
 }
