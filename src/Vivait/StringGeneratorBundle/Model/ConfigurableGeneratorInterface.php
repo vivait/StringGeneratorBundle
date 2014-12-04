bc249e25a230841608899021808ccf324f8a4907
Robin Cawser <robin@vivait.co.uk>
2014-12-04 16:05:56 +0000

    Add documentation to ConfigurableInterface

--- src/Vivait/StringGeneratorBundle/Model/ConfigurableGeneratorInterface.php
+++ src/Vivait/StringGeneratorBundle/Model/ConfigurableGeneratorInterface.php
@@ -6,7 +6,15 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 interface ConfigurableGeneratorInterface extends GeneratorInterface
 {
+    /**
+     * @param array $options
+     * @return mixed
+     */
     public function setOptions(array $options = []);
 
+    /**
+     * @param OptionsResolver $resolver
+     * @return mixed
+     */
     public function getDefaultOptions(OptionsResolver $resolver);
 }
