878955de31b305338302562f27122658380b5ec4
David Pérez Terol <davidtxnet@gmail.com>
2017-03-31 18:14:54 +0200

    Fix SecureByterGeneratorSpec test to random_bytes

--- spec/Vivait/StringGeneratorBundle/Generator/SecureBytesGeneratorSpec.php
+++ spec/Vivait/StringGeneratorBundle/Generator/SecureBytesGeneratorSpec.php
@@ -4,19 +4,12 @@ namespace spec\Vivait\StringGeneratorBundle\Generator;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
-use Symfony\Component\Security\Core\Util\SecureRandom;
 
 class SecureBytesGeneratorSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType('Vivait\StringGeneratorBundle\Generator\SecureBytesGenerator');
-    }
-
-    function let()
-    {
-        $secureRandom = new SecureRandom();
-        $this->beConstructedWith($secureRandom);
     }
 
     function it_generates_random_string()
